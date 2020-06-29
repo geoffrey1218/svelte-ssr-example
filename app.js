2e8555f6da8629b554800121b1f2f2d9656354a5
Geoff Rich <Geoff.Rich@alaskaair.com>
2020-06-29 09:03:37 -0700

    Working basic SSR

--- app.js
+++ app.js
@@ -2,7 +2,35 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+require('svelte/register');
+const App = require('./src/App.svelte').default;
+
+const template = (head, html, css) => `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset='utf-8'>
+        <meta name='viewport' content='width=device-width,initial-scale=1'>
+
+        <title>Svelte app</title>
+
+        <link rel='icon' type='image/png' href='/favicon.png'>
+        <link rel='stylesheet' href='/global.css'>
+        ${head}
+        <style>${css.code}</style>
+    </head>
+
+    <body>${html}</body>
+    </html>
+`;
+
 app.use(express.static("public"));
-app.get("/", (req, res) => res.send("Hello World!"));
+app.get("/ssr", (req, res) => {
+    const { head, html, css } = App.render({
+        name: 'World'
+    });
+    console.log(css);
+    res.send(template(head, html, css));
+});
 
 app.listen(port, () => console.log(`Listening at http://localhost:${port}`));
